d544ab6cbe6c614f473bdecbffd9261ec0a2608a
iejMac <kilianmaciej6@gmail.com>
2021-10-30 14:59:30 -0700

    train: train script with data

--- train.py
+++ train.py
@@ -1,33 +1,30 @@
 import os
-import model
+import numpy as np
 
 import torch
 from torch import nn
 from torch import optim
 
+from model import SimpleModel
+
 EPOCHS = 100
+DATA_DIR = "data"
 
 if __name__ == "__main__":
 
-  mod = model.SimpleModel(5, 5)
-  x = torch.randn((3, 1, 520, 704))
-  label = torch.zeros((3, 1, 520, 704))
-  label[:, :, 20:40, 100:200] = 1.0
+  mod = SimpleModel(10, 10)
+
+  Xs = torch.tensor(np.load(os.path.join(DATA_DIR, "train_X.npy")).reshape((606, 1, 520, 704)).astype(np.float32))
+  ys = torch.tensor(np.load(os.path.join(DATA_DIR, "train_y.npy")).reshape((606, 1, 520, 704)).astype(np.float32))
 
   loss_fn = nn.BCEWithLogitsLoss()
   opt = optim.Adam(mod.parameters(), lr=3e-4,betas=(0.9,0.98),eps=1e-6,weight_decay=0.0)
 
   for e in range(EPOCHS):
-    logits = mod(x)
+    logits = mod(Xs)
 
-    loss = loss_fn(logits, label)
+    loss = loss_fn(logits, ys)
     print(loss)
     opt.zero_grad()
     loss.backward()
     opt.step()
-
-
-
-
-
-
